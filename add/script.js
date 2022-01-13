8bf02e26059b1c18ae8f23b8a01e0e556ca38601
dr-Andrei <bitchandrei@yandex.ru>
2022-01-13 16:49:55 +0300

    update add

---
+++ add/script.js
@@ -0,0 +1,20 @@
+let div_out = document.querySelector('.massValues');
+let button_add;
+
+let object = '<div class="valueElement"><label>Термин / предложение <input type="text" class="fouEnglish" placeholder="по-английски"><input type="text" class="fouRussian" placeholder="по-русски"></label><button class="add">Добавить значение</button></div>';
+
+draw();
+
+//функция отрисовки на странице
+function draw (data = object) {
+    let div_out_draw = document.querySelector('.massValues').innerHTML;
+
+    div_out.innerHTML = div_out_draw + data;
+    
+    let button_arr = document.querySelectorAll('.add');
+    for(let i of button_arr) {
+        i.onclick = () => {
+            draw();
+         }
+    }  
+}
