57b77ad1856fed54e0baa5cd21f9082f3be8efe1
dr-Andrei <bitchandrei@yandex.ru>
2022-01-13 16:50:45 +0300

    update add

--- add/script.js
+++ add/script.js
@@ -8,9 +8,9 @@ draw();
 //функция отрисовки на странице
 function draw (data = object) {
     let div_out_draw = document.querySelector('.massValues').innerHTML;
-
     div_out.innerHTML = div_out_draw + data;
-    
+
+    //функция добавления нового поля по клику
     let button_arr = document.querySelectorAll('.add');
     for(let i of button_arr) {
         i.onclick = () => {
